d9f389287753627bdfbacd97188e72f44b9f9251
4lexrossi <devalexrossi@gmail.com>
2026-05-07 21:21:40 -0300

    feat: implement authentication module with Firebase integration and JWT support

---
+++ src/firebase/firebase.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as admin from 'firebase-admin';
+
+@Injectable()
+export class FirebaseService implements OnModuleInit {
+  private firestore: admin.firestore.Firestore;
+
+  constructor(private configService: ConfigService) { }
+
+  onModuleInit() {
+    if (admin.apps.length === 0) {
+      const serviceAccountPath = './firebase-service-account.json';
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccountPath),
+      });
+    }
+    this.firestore = admin.firestore();
+  }
+
+  getDb() {
+    return this.firestore;
+  }
+}
